6c7a67b3c22a8f6b5a58d256780ce841e24ee1f0
yahia benotmane <benotmane.yahia@univ-bechar.dz>
2024-02-14 17:49:44 +0100

    commit

---
+++ app/src/main/java/com/example/mygsm/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.mygsm;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText phoneNumberEditText;
+    private static final int SMS_PERMISSION_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Switch smsSwitch = findViewById(R.id.smsSwitch);
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+
+        smsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                String number = phoneNumberEditText.getText().toString();
+                // number = "0657105771";
+
+                if (number.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "No number , please try again.", Toast.LENGTH_LONG).show();
+
+                }else {
+                    if (isChecked) {
+                        sendSMS("on", number);
+                    } else {
+                        sendSMS("off",number);
+                    }
+                }
+
+
+            }
+        });
+
+        //  permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            //  not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    SMS_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void sendSMS(String message, String phoneNumber) {
+        String smsBody = message;
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, smsBody, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case SMS_PERMISSION_REQUEST_CODE: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission granted
+                    Toast.makeText(this, "SMS Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Permission denied
+                    Toast.makeText(this, "SMS Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+}
